39382c916dc1721173bdf223a4cd70c896f31a2c
yemliha81 <yemlihaddln@hotmail.com>
2025-12-07 22:27:26 +0300

    min person

--- resources/views/teacher/dashboard.blade.php
+++ resources/views/teacher/dashboard.blade.php
@@ -90,7 +90,7 @@
 
                     <div class="mb-3">
                     <label>Minimum Katılımcı</label>
-                    <input type="number" id="min_person" class="form-control" min="1">
+                    <input type="number" id="min_person" min="5"  class="form-control" min="1">
                     </div>
 
                     <div class="mb-3">
