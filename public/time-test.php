5956792f67e4792ce9dce3e44ed6f3b3e8d2936c
yemliha81 <yemlihaddln@hotmail.com>
2025-12-14 09:22:29 +0300

    test

--- public/time-test.php
+++ public/time-test.php
@@ -1,4 +1,8 @@
-<?php 
-    date_default_timezone_set('Europe/Istanbul');
-    echo date('Y-m-d H:i:s'); 
+<?php
+echo date_default_timezone_get() . "
+";
+date_default_timezone_set('Europe/Istanbul');
+echo date_default_timezone_get() . "
+";
+echo date('Y-m-d H:i:s');
 ?>
