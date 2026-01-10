a93a2fe0e3ceb72e92e12e98a59615e5d639661b
yemliha81 <yemlihaddln@hotmail.com>
2026-01-10 19:59:19 +0300

    Teacher stats

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -70,7 +70,7 @@ class HomeController extends Controller
             //where start is smaller than today
             ->where('start', '<=', date('Y-m-d H:i:s'))
             ->count();
-            $teacherArray[] = [$teacher->id . '|' . $teacher->name. '|' . $teacher->branch, $teacher->event_count];
+            $teacherArray[] = [$teacher->name. '|' . $teacher->branch, $teacher->event_count];
         }
         // sort by event_count desc
         usort($teacherArray, function($a, $b) {
